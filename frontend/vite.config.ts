8f650b6253650468bbea6772c558aa9398dc5039
dcsuibian <dcsuibian@163.com>
2024-07-14 16:37:39 +0800

    增加.gitignore、backend、frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 7310,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7311',
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+})
