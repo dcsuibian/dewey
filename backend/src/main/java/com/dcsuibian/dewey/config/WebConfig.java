0006fdd05df501b1dc6934fef07ae98bde975fad
dcsuibian <dcsuibian@163.com>
2024-07-15 03:13:30 +0800

    后台配合前台单页路由

--- backend/src/main/java/com/dcsuibian/dewey/config/WebConfig.java
+++ backend/src/main/java/com/dcsuibian/dewey/config/WebConfig.java
@@ -1,17 +1,30 @@
 package com.dcsuibian.dewey.config;
 
 import com.dcsuibian.dewey.interceptor.SessionInterceptor;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer, ErrorPageRegistrar {
     /**
      * 添加拦截器
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SessionInterceptor());
+    }
+
+    /**
+     * 配合前台单页路由
+     */
+    @Override
+    public void registerErrorPages(ErrorPageRegistry registry) {
+        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/");
+        registry.addErrorPages(error404Page);
     }
 }
